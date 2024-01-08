c2d0f3dcd3cb45e986a46efb5faddbc5c7e363fa
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-01-08 14:55:32 +0200

    4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,6 +11,23 @@ const favoriteBlog = (blogs) => {
   return blogs.find(blog => blog.likes === maxLikes)
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+  const authorCount = new Map()
+  blogs.forEach(blog => {
+    const author = blog.author
+    authorCount.set(author, (authorCount.get(author) || 0) + 1)
+  })
+  let [maxBlogs, mostBlogsAuthor] = [0, null]
+  for (const [author, count] of authorCount) {
+    if (count > maxBlogs) {
+      maxBlogs = count
+      mostBlogsAuthor = author
+    }
+  }
+  return { author: mostBlogsAuthor, blogs: maxBlogs }
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
