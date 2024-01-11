01e5a63e0ef715c017a01c82914fa04d24508e44
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-01-11 15:11:51 +0200

    4.15

---
+++ controllers/users.js
@@ -0,0 +1,30 @@
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user.js')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).select('username name _id')
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  if (!body.password || body.password.length < 3) {
+    return response.status(400).send('Password must be at least 3 characters long')
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const newUser = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash: passwordHash
+  })
+
+  const savedUser = await newUser.save()
+  response.json(savedUser)
+})
+
+module.exports = usersRouter
